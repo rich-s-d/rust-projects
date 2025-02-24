974fd69fa2ca5c87d338a664ca4da552e8282441
Shane Rich <srichnz@gmail.com>
2025-02-24 17:36:50 +0100

    Added unit test for case_insensitve and function search_case_insensitive using red green refactoring.

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -33,7 +33,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> { //using () like this 
 mod tests {
     use super::*;
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "/
 Rust:
@@ -42,13 +42,40 @@ Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
+
+    #[test]
+    fn case_insensitve() {
+        let query = "ruSt";
+        let contents = "/
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me!";
+
+        assert_eq!(vec!["Rust:", "Trust me!"],
+        search_case_insensitive(query, contents))
+    }
 }
 
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> { // 'a, Recall in Chapter 10 that the lifetime parameters specify which argument lifetime is connected to the lifetime of the return value. In this case, we indicate that the returned vector should contain string slices that reference slices of the argument contents (rather than the argument query).
     let mut results = Vec::new();
 
     for line in contents.lines() {
         if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
             results.push(line);
         }
     }
